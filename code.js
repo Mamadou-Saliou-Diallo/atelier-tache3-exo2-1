8adf15e1d905aece17d6c9dd5b7a7cb8b85fd271
Mamadou Saliou DIALLO <kewere097@gmail.com>
2024-10-03 13:27:32 +0000

    first commit

---
+++ code.js
@@ -0,0 +1,10 @@
+// Déclaration des variables
+let a = 5; // Tu peux changer ces valeurs
+let b = 10;
+let c = 2;
+
+// Calcul de la somme
+let somme = a + b + c;
+
+// Affichage du résultat dans la console
+console.log("La somme des trois variables est :", somme);
